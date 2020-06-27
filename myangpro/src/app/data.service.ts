e2ce1c0291064d0fd9820cf21944f0609c99b53f
YASH SHARMA <sharmayash534@gmail.com>
2020-06-27 11:43:44 +0530

    yash

--- myangpro/src/app/data.service.ts
+++ myangpro/src/app/data.service.ts
@@ -5,25 +5,16 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-
-
-  constructor(private http:HttpClient) { }
-
-
+  
+ constructor(private http:HttpClient) { }
   getPosts():any
   {
-
-     return this.http.get('https://jsonplaceholder.typicode.com/posts');
-     
+     return this.http.get('https://jsonplaceholder.typicode.com/posts');  
   }
-
-
   signUp(d):any
   {
     return this.http.post('http://localhost:3000/sign-up', d);
   }
-
-
   signIn(d):any
   {
     return this.http.post('http://localhost:3000/sign-in', d);
