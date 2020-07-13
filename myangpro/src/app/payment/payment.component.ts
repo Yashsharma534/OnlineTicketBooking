0f704731fc371f7ccbb3a0919de97e3bd52c7738
YASH SHARMA <sharmayash534@gmail.com>
2020-07-13 22:42:15 +0530

    yash

--- myangpro/src/app/payment/payment.component.ts
+++ myangpro/src/app/payment/payment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PaymentComponent implements OnInit {
   total;
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((d)=>{
@@ -18,5 +18,9 @@ this.total = d.get('total');
 payment()
 {
   alert("payment done successfully");
+}
+logout()
+{
+  this.router.navigate(['/']);
 }
 }
